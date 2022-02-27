3058d2e4eac47a382f391f34543df2720d10b7e1
Aayushbansal12345 <99741804+Aayushbansal12345@users.noreply.github.com>
2022-02-28 02:31:13 +0530

    Add files via upload

---
+++ pr_4.py
@@ -0,0 +1,6 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = [1,2,[3,4],[5,[100,200,['hello']],23,11],1,7]
+>>> a[3][1][2]
+['hello']
+>>> 
---
+++ pr_5.py
@@ -0,0 +1,8 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = [1,2,[3,4],[5,[100,200,['hello']],23,11],1,7]
+>>> a.insert(0,'s1')
+>>> a.insert(7,'s2')
+>>> a
+['s1', 1, 2, [3, 4], [5, [100, 200, ['hello']], 23, 11], 1, 7, 's2']
+>>> 
